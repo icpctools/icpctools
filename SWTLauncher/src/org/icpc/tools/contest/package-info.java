37e347cb989d391bfdaf1e86955f59ccb2f6cc9a
deboer-tim <deboer@ca.ibm.com>
2019-05-07 08:49:35 -0400

    Build simplification

--- SWTLauncher/src/org/icpc/tools/contest/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains a SWTLauncher that can launch other classes that
- * require SWT, but passing in the jar the class is contained in and will locate
- * the proper SWT jar for that arch/os name.
- *
- * @author Tim deBoer
- */
-package org.icpc.tools.contest;
