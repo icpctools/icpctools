6178930a4666053898706e69c5b70f12e5ee863d
Tim deBoer <git@tdeboer.ca>
2020-02-25 23:29:07 -0500

    Use contest time output from presentations
    Copied the contest time utility method from presentations to make things completely consistent, removed oddly named getTimeDeltaNano() since it isn't being used after this pass.

--- PresCore/src/org/icpc/tools/client/core/BasicClient.java
+++ PresCore/src/org/icpc/tools/client/core/BasicClient.java
@@ -599,10 +599,6 @@ public class BasicClient {
 		nanoTimeDelta = newNanoTimeDelta;
 	}
 
-	protected long getTimeDeltaNano() {
-		return nanoTimeDelta;
-	}
-
 	private static void trace(String message, boolean user) {
 		String s = message;
 		if (s.length() > 150)
