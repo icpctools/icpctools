71ca36eb6be56c0688adbd185cf2ba39ef698e18
Tim deBoer <git@tdeboer.ca>
2021-08-24 17:09:22 -0400

    Change formatting for pdf
    Local tests indicate that these two changes should improve the parsing of the git markdown and likely fix the table rendering in the PresContest docs

--- ContestModel/doc.sh
+++ ContestModel/doc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 1 - $1
-pandoc "$1"/README.md -f markdown-implicit_figures -V linkcolor:blue -V geometry:margin=1in -o "$1"/README.pdf
+pandoc "$1"/README.md -f gfm -V linkcolor:blue -V geometry:margin=1in -o "$1"/README.pdf
 echo 2
