1b62ab1d0f212cdc4cb53ac556c9989755f33263
Tim deBoer <git@tdeboer.ca>
2021-09-19 10:51:12 -0400

    2020 floor map
    Updated to match the current floor diagram, along with a couple minor fixes

--- ContestUtil/src/org/icpc/tools/contest/util/floor/FloorGenerator2020.java
+++ ContestUtil/src/org/icpc/tools/contest/util/floor/FloorGenerator2020.java
@@ -5,21 +5,22 @@ import org.icpc.tools.contest.model.FloorMap;
 import org.icpc.tools.contest.model.FloorMap.Path;
 import org.icpc.tools.contest.model.IPrinter;
 import org.icpc.tools.contest.model.ITeam;
+import org.icpc.tools.contest.model.internal.Team;
 
 public class FloorGenerator2020 extends FloorGenerator {
 	// table width (in meters). ICPC standard is 1.8
-	private static final float tw = 1.8f;
+	private static final float tw = 3.3f;
 
-	// table depth (in meters). ICPC standard is 0.8
-	private static final float td = 0.8f;
+	// table depth (in meters). ICPC standard is 0.85
+	private static final float td = 0.95f;
 
 	// team area width (in meters). ICPC standard is 3.0
-	private static final float taw = 2.2f;
+	private static final float taw = 3.3f;
 
 	// team area depth (in meters). ICPC standard is 2.2
-	private static final float tad = 2.0f;
+	private static final float tad = 2.5f;
 
-	private static final float aisle = 3.75f + td;
+	private static final float aisle = 2f + tad * 3 / 2;
 
 	private static FloorMap floor = new FloorMap(taw, tad, tw, td);
 
@@ -49,6 +50,19 @@ public class FloorGenerator2020 extends FloorGenerator {
 		floor.createTeam(newId, t.getX() + dx, t.getY() + dy, t.getRotation());
 	}
 
+	protected static float createAisleOfTeams(int teamNumber, float xx, float y) {
+		float x = xx;
+		x += aisle / 2;
+		createTeamRow(6, teamNumber, x, y, 0, taw, FloorMap.E);
+		x += tad / 2;
+		createTeamRowRev(6, teamNumber + 11, x, y, 0, taw, FloorMap.W);
+		x += aisle / 2;
+
+		floor.createAisle(x, y - taw, x, y + taw * 6);
+
+		return x;
+	}
+
 	public static void main(String[] args) {
 		Trace.init("ICPC Floor Map Generator", "floorMap", args);
 
@@ -56,69 +70,35 @@ public class FloorGenerator2020 extends FloorGenerator {
 			float x = 0;
 			float y = 0;
 
-			float fx = tad * 7 / 2 + aisle * 7;
-			floor.createAisle(x, y - taw, x + fx, y - taw);
-			floor.createAisle(x, y + taw * 10, x + fx, y + taw * 10);
+			floor.createAisle(x, y - taw, x, y + taw * 6);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			x = createAisleOfTeams(0, x, y);
 
-			x += aisle / 2;
-			createTeamRow(10, 1, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 20, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			x = createAisleOfTeams(12, x, y);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			x = createAisleOfTeams(24, x, y);
 
-			x += aisle / 2;
-			createTeamRow(10, 21, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 40, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			x = createAisleOfTeams(36, x, y);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			x = createAisleOfTeams(48, x, y);
 
-			x += aisle / 2;
-			createTeamRow(10, 41, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 60, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			x = createAisleOfTeams(60, x, y);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			x = createAisleOfTeams(72, x, y);
 
-			x += aisle / 2;
-			createTeamRow(10, 61, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 80, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			x = createAisleOfTeams(84, x, y);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			x = createAisleOfTeams(96, x, y);
 
-			x += aisle / 2;
-			createTeamRow(10, 81, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 100, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			x = createAisleOfTeams(108, x, y);
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
+			floor.createAisle(0, -taw, x, -taw);
+			floor.createAisle(0, taw * 6, x, taw * 6);
 
-			x += aisle / 2;
-			createTeamRow(10, 101, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 120, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
+			((Team) floor.getTeam(0)).add("id", "-1");
+			((Team) floor.getTeam(119)).add("id", "-1");
 
-			floor.createAisle(x, y - taw, x, y + taw * 10);
-
-			x += aisle / 2;
-			createTeamRow(10, 121, x, y, 0, taw, FloorMap.E);
-			x += tad / 2;
-			createTeamRowRev(10, 140, x, y, 0, taw, FloorMap.W);
-			x += aisle / 2;
-
-			floor.createAisle(x, y - taw, x, y + taw * 10);
-
-			IPrinter p = floor.createPrinter(x, y - taw - 2);
+			IPrinter p = floor.createPrinter(x - taw, y - taw - 2);
 
 			double bx = 0;
 			for (int i = 0; i < 11; i++)
@@ -136,11 +116,11 @@ public class FloorGenerator2020 extends FloorGenerator {
 			Path path1 = floor.getPath(t1, t2);
 			Path path2 = floor.getPath(t1, p);
 
-			System.out.println("left: " + floor.getTeamToLeftOf(floor.getTeam(49)).getId());
+			/*System.out.println("left: " + floor.getTeamToLeftOf(floor.getTeam(49)).getId());
 			System.out.println("right: " + floor.getTeamToRightOf(floor.getTeam(49)).getId());
-
+			
 			System.out.println("left: " + floor.getTeamToLeftOf(floor.getTeam(32)).getId());
-			System.out.println("right: " + floor.getTeamToRightOf(floor.getTeam(32)).getId());
+			System.out.println("right: " + floor.getTeamToRightOf(floor.getTeam(32)).getId());*/
 
 			Trace.trace(Trace.USER, "Time: " + (System.currentTimeMillis() - time));
 
