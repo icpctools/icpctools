a14fe7f7678a513286bc86b51d9a446b5d8ecf08
Mattias de Zalenski <dezalenski@gmail.com>
2021-10-02 11:09:22 +0300

    Add diff and 1 second time sync lines to test sync
    Show server time difference.

--- PresContest/src/org/icpc/tools/presentation/contest/internal/presentations/test/TestSyncPresentation.java
+++ PresContest/src/org/icpc/tools/presentation/contest/internal/presentations/test/TestSyncPresentation.java
@@ -1,8 +1,10 @@
 package org.icpc.tools.presentation.contest.internal.presentations.test;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 
 import org.icpc.tools.presentation.core.Presentation;
 
@@ -16,7 +18,13 @@ public class TestSyncPresentation extends Presentation {
 	}
 
 	@Override
-	public void paint(Graphics2D g2) {
+	public void paint(Graphics2D g) {
+		paintSyncLine(g);
+		paintTimeDiff(g);
+		paintOval(g);
+	}
+
+	void paintOval(Graphics2D g2) {
 		float t = (getRepeatTimeMs() % 2000L) / 500f;
 
 		Graphics2D g = (Graphics2D) g2.create();
@@ -40,10 +48,43 @@ public class TestSyncPresentation extends Presentation {
 		float x = (d.width - 50) * xx;
 		float y = (d.height - 50) * yy;
 
-		g.translate(x, y);
 		g.setColor(isLightMode() ? Color.ORANGE : Color.YELLOW);
 
+		g.translate(x, y);
 		g.fillOval(0, 0, 50, 50);
+
 		g.dispose();
+	}
+
+	void paintSyncLine(Graphics2D g) {
+		long ms = getTimeMs();
+		int x = (int) (ms % 1000) * width / 1000;
+
+		g.setColor(isLightMode() ? Color.LIGHT_GRAY : Color.DARK_GRAY);
+		g.fillRect(x, 0, 10, height);
+		float t = (getRepeatTimeMs() % 2000L) / 500f;
+	}
+
+	long avgMilliDiff;
+	int avgMilliDiffCount;
+
+	void paintTimeDiff(Graphics2D g) {
+		long ms = getTimeMs();
+		long localMs = System.currentTimeMillis();
+		long milliDiff = localMs - ms;
+		avgMilliDiff = (avgMilliDiff * avgMilliDiffCount + milliDiff) / (avgMilliDiffCount + 1);
+		if (avgMilliDiffCount < 10) {
+			avgMilliDiffCount++;
+		}
+		double a = Math.atan(avgMilliDiff / 1000.0);
+
+		String s = "" + avgMilliDiff;
+		int sw = g.getFontMetrics().stringWidth(s), sh = g.getFontMetrics().getHeight();
+
+		g.setColor(isLightMode() ? Color.LIGHT_GRAY : Color.DARK_GRAY);
+		g.setStroke(new BasicStroke(2));
+		int x = width - 30, y = height - 50;
+		g.draw(new Line2D.Double(x, y, x + Math.sin(a) * 25, y - Math.cos(a) * 25));
+		g.drawString(s, x - sw / 2, y + sh);
 	}
 }
