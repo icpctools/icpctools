e2f7de309fcce9bdbec4c01e5ddcc15c5b100285
Nicky Gerritsen <nickygerritsen@me.com>
2022-03-09 09:57:15 +0100

    Improvements to the CMS importer:
    - Actually use the paths you supply at the CLI instead of hardcoded paths. Also add argument for CMS contest root (for Person.tab/TeamPerson.tab files)
    - Retry Google Maps API without country code if it does't work with country code. Example: Universidad de Murcia ESP vs Universidad de Murcia
    - Add option to assign team ID's in order. Non-WF contests often have all teams have ID 0 in the CMS. If the contest doesn't care about actual numbers, this will assign 1- n to them

--- ContestUtil/src/org/icpc/tools/contest/util/cms/GoogleMapsGeocoder.java
+++ ContestUtil/src/org/icpc/tools/contest/util/cms/GoogleMapsGeocoder.java
@@ -23,7 +23,41 @@ public class GoogleMapsGeocoder {
 
 	public void geocode(Organization org) {
 		try {
-			String address = URLEncoder.encode(org.getFormalName() + " " + org.getCountry(), "UTF-8");
+			String address = org.getFormalName() + " " + org.getCountry();
+			Object[] results = geocodeResultsFor(address);
+			if (results.length > 1) {
+				System.err.println(
+						"Geocode for '" + address + "' returned " + results.length + " results, using first one");
+			} else if (results.length == 0) {
+				System.err.println("Geocode for '" + address + "' returned no results, retrying without country");
+				address = org.getFormalName();
+				results = geocodeResultsFor(address);
+				if (results.length > 1) {
+					System.err.println(
+							"Geocode for '" + address + "' returned " + results.length + " results, using first one");
+				} else if (results.length == 0) {
+					System.err.println("Geocode for '" + address + "' returned no results");
+					return;
+				}
+			}
+
+			JsonObject result = (JsonObject) results[0];
+			JsonObject location = result.getJsonObject("geometry").getJsonObject("location");
+			String lat = location.getString("lat");
+			String lon = location.getString("lng");
+			JsonObject obj = new JsonObject();
+			obj.props.put("latitude", lat);
+			obj.props.put("longitude", lon);
+			org.add("location", obj);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected Object[] geocodeResultsFor(String address)
+	{
+		try {
+			address = URLEncoder.encode(address, "UTF-8");
 			String fullUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + apiKey;
 
 			HttpURLConnection conn = (HttpURLConnection) (new URL(fullUrl)).openConnection();
@@ -31,7 +65,7 @@ public class GoogleMapsGeocoder {
 			conn.setRequestMethod("GET");
 
 			if (conn.getResponseCode() != 200)
-				return;
+				return null;
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String inputLine;
@@ -45,28 +79,13 @@ public class GoogleMapsGeocoder {
 			JsonObject json = (new JSONParser(content.toString())).readObject();
 
 			if (!json.containsKey("results")) {
-				return;
+				return null;
 			}
 
-			Object[] results = json.getArray("results");
-			if (results.length > 1) {
-				System.err.println(
-						"Geocode for " + org.getName() + " returned " + results.length + " results, using first one");
-			} else if (results.length == 0) {
-				System.err.println("Geocode for " + org.getName() + " returned no results");
-				return;
-			}
-
-			JsonObject result = (JsonObject) results[0];
-			JsonObject location = result.getJsonObject("geometry").getJsonObject("location");
-			String lat = location.getString("lat");
-			String lon = location.getString("lng");
-			JsonObject obj = new JsonObject();
-			obj.props.put("latitude", lat);
-			obj.props.put("longitude", lon);
-			org.add("location", obj);
+			return json.getArray("results");
 		} catch (IOException | IllegalArgumentException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 }
