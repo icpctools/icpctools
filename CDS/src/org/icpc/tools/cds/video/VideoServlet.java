7d585540fd394e3871633cdd7c12a4597cfcef71
Tim deBoer <git@tdeboer.ca>
2022-03-23 07:42:34 -0400

    Add nginx non-buffering header
    Avoid nginx buffering streaming responses. User could turn this off globally on nginx, but better they don't even have to know about it.

--- CDS/src/org/icpc/tools/cds/video/VideoServlet.java
+++ CDS/src/org/icpc/tools/cds/video/VideoServlet.java
@@ -183,6 +183,7 @@ public class VideoServlet extends HttpServlet {
 		response.setHeader("Cache-Control", "no-cache");
 		response.setContentType("application/octet");
 		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("X-Accel-Buffering", "no");
 
 		OutputStream out = response.getOutputStream();
 		va.handler.writeHeader(out, stream);
