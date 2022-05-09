fa65a1eefa386e5662cdd2347d3516f27a786f2d
Tim deBoer <git@tdeboer.ca>
2022-05-09 11:40:47 -0400

    Notifications event feed
    Support for the new event feed notification format (no 'op', notifications can be single object or arrays), without dropping support for the previous event feed format.

--- ContestModel/src/org/icpc/tools/contest/model/feed/NDJSONFeedLogParser.java
+++ ContestModel/src/org/icpc/tools/contest/model/feed/NDJSONFeedLogParser.java
@@ -8,16 +8,20 @@ import java.io.InputStreamReader;
 import org.icpc.tools.contest.Trace;
 import org.icpc.tools.contest.model.feed.JSONParser.JsonObject;
 
+/**
+ * Helper class to quickly scan through an event feed and pick up the last token or (for old event
+ * feed format) the event id.
+ */
 public class NDJSONFeedLogParser {
 	protected String lastId;
+	protected String lastToken;
 	protected String firstComment;
+	protected Boolean isOldFeed;
 
 	public void parse(InputStream in) throws Exception {
 		if (in == null)
 			return;
 
-		// {"event": "<event type>", "id": "<id>", "op":"create/update/delete" "data": <data from
-		// endpoint> }
 		String s = null;
 		String lastJson = null;
 		try {
@@ -39,18 +43,28 @@ public class NDJSONFeedLogParser {
 
 				JSONParser rdr = new JSONParser(lastJson);
 				JsonObject obj = rdr.readObject();
-				lastId = obj.getString("id");
+				if (isOldFeed == null)
+					isOldFeed = obj.getString("op") != null;
+
+				if (isOldFeed)
+					lastId = obj.getString("id");
+				else if (obj.getString("token") != null)
+					lastToken = obj.getString("token");
 			} catch (Exception e) {
 				Trace.trace(Trace.ERROR, "Could not parse event feed log " + s, e);
 			}
 		} catch (Exception e) {
-			Trace.trace(Trace.ERROR, "Could not parse event feedlog " + s, e);
+			Trace.trace(Trace.ERROR, "Could not parse event feed log " + s, e);
 			throw new IOException("Error parsing event feed");
 		}
 	}
 
 	public String getLastEventId() {
 		return lastId;
+	}
+
+	public String getLastToken() {
+		return lastToken;
 	}
 
 	public String getFirstComment() {
