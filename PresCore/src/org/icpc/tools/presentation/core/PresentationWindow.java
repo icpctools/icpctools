99401a20c16f9dafb330cf69700e2350fcedd407
Mattias de Zalenski <dezalenski@gmail.com>
2022-11-10 04:44:01 +0100

    Draw performance timer spark lines

--- PresCore/src/org/icpc/tools/presentation/core/PresentationWindow.java
+++ PresCore/src/org/icpc/tools/presentation/core/PresentationWindow.java
@@ -88,8 +88,18 @@ public abstract class PresentationWindow extends Frame implements IPresentationH
 						&& (e.isControlDown() || e.isShiftDown()))
 					System.exit(0);
 
-				if (KeyEvent.VK_D == e.getKeyCode() && (e.isControlDown() || e.isShiftDown()))
+				if (KeyEvent.VK_D == e.getKeyCode() && (e.isControlDown() || e.isShiftDown())) {
 					toggleDebug();
+					boolean showStats = false, showSparklines = false;
+					if (e.isControlDown()) {
+						showStats = true;
+					}
+					if (e.isControlDown() && e.isShiftDown()) {
+						showSparklines = true;
+					}
+					setShowTimerStats(showStats);
+					setShowTimerSparklines(showSparklines);
+				}
 
 				if (KeyEvent.VK_C == e.getKeyCode() && (e.isControlDown() || e.isShiftDown()))
 					clearCaches();
@@ -194,4 +204,12 @@ public abstract class PresentationWindow extends Frame implements IPresentationH
 	public abstract void setLightMode(boolean light);
 
 	public abstract void toggleDebug();
+
+	public abstract void setShowTimerStats(boolean show);
+
+	public abstract void setShowTimerSparklines(boolean show);
+
+	public abstract void clearCaches();
+
+	public abstract void resetTime();
 }
