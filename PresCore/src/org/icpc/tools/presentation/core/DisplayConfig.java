6f62bf82bc0bbb6299779b9d67aa404972addaae
Tim deBoer <git@tdeboer.ca>
2023-05-25 16:38:07 -0400

    Make visibility issue Mac specific, add test display graphics mode
    Make visibility issue Mac specific, add 't' mode for testing that will allow you to pick any display config

--- PresCore/src/org/icpc/tools/presentation/core/DisplayConfig.java
+++ PresCore/src/org/icpc/tools/presentation/core/DisplayConfig.java
@@ -1,33 +1,38 @@
 package org.icpc.tools.presentation.core;
 
+/**
+ * a,b,c,d - quadrants
+ *
+ * e, f = medium, bl
+ *
+ * m - middle
+ *
+ * l - almost full screen
+ *
+ * w - full screen window
+ *
+ * x - full screen max
+ *
+ * t - full screen picking config
+ */
 public class DisplayConfig {
 	public enum Mode {
-		TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, MEDIUM, MEDIUM_BL, MIDDLE, ALMOST, FULL_WINDOW, FULL_SCREEN_MAX, FULL_SCREEN
+		TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, MEDIUM, MEDIUM_BL, MIDDLE, ALMOST, FULL_WINDOW, FULL_SCREEN_MAX, FULL_SCREEN_T, FULL_SCREEN
 	}
 
 	public int device;
 	public Mode mode = Mode.FULL_SCREEN;
+	public int conf = -1;
 
 	public int id = -1; // an identifier for when there are multiple multi-screens
 	public int pos; // position in the grid. 0 is top left, counting to the right
 	public int ww; // number of screens wide
 	public int hh; // number of screens high
 
-	private char[] PosStrs = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'm', 'l', 'w', 'x' };
+	private char[] PosStrs = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'm', 'l', 'w', 'x', 't' };
 
-	public DisplayConfig(String displayStr) {
-		if (displayStr == null)
-			return;
-
-		device = Integer.parseInt(displayStr.charAt(0) + "") - 1;
-
-		mode = Mode.FULL_SCREEN;
-		if (displayStr.length() > 1) {
-			char c = displayStr.charAt(1);
-			for (int i = 0; i < PosStrs.length; i++)
-				if (PosStrs[i] == c)
-					mode = Mode.values()[i];
-		}
+	public DisplayConfig(String display) {
+		this(display, null);
 	}
 
 	/**
@@ -43,11 +48,19 @@ public class DisplayConfig {
 			}
 
 			mode = Mode.FULL_SCREEN;
-			if (display.length() == 2) {
+			if (display.length() > 1) {
 				char c = display.charAt(1);
 				for (int i = 0; i < PosStrs.length; i++)
 					if (PosStrs[i] == c)
 						mode = Mode.values()[i];
+			}
+
+			if (display.length() > 2 && mode == Mode.FULL_SCREEN_T) {
+				try {
+					conf = Integer.parseInt(display.substring(2)) - 1;
+				} catch (Exception e) {
+					throw new IllegalArgumentException("Invalid display argument, should be a digit");
+				}
 			}
 		}
 
@@ -93,6 +106,7 @@ public class DisplayConfig {
 		System.out.println(new DisplayConfig("1", "1@2x2"));
 		System.out.println(new DisplayConfig("1b", "2@2x2d"));
 		System.out.println(new DisplayConfig("1x", "4@3x3b"));
+		System.out.println(new DisplayConfig("1t5"));
 	}
 
 	@Override
