79a6ee8cdff5e214039b45310475e71de13b8424
Mattias de Zalenski <dezalenski@gmail.com>
2024-04-15 09:24:36 +0200

    Add composite presentations in single dialog

--- PresAdmin/src/org/icpc/tools/presentation/admin/internal/CompositePresentationDialog.java
+++ PresAdmin/src/org/icpc/tools/presentation/admin/internal/CompositePresentationDialog.java
@@ -19,7 +19,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.icpc.tools.contest.Trace;
 import org.icpc.tools.presentation.admin.internal.PresentationInfoListControl.DisplayStyle;
 import org.icpc.tools.presentation.core.internal.PresentationInfo;
 
@@ -31,9 +30,8 @@ public class CompositePresentationDialog extends Dialog {
 	private PresentationInfoListControl transitionList;
 	private PresentationInfoListControl buildList;
 
-	protected Button ok;
-	protected boolean save;
 	protected String name, description, category;
+	protected CompositePresentationSaveDialog saveDialog;
 
 	public CompositePresentationDialog(Shell parent, List<PresentationInfo> presentations,
 			List<PresentationInfo> transitions) {
@@ -87,6 +85,12 @@ public class CompositePresentationDialog extends Dialog {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
+
+		boolean save = saveDialog.isSaveOk();
+		name = saveDialog.getName();
+		description = saveDialog.getDescription();
+		category = saveDialog.getCategory();
+
 		return save;
 	}
 
@@ -100,8 +104,19 @@ public class CompositePresentationDialog extends Dialog {
 		createTransitionSection(comp);
 		createDisplayPlanSection(comp);
 
-		// buttons
-		Composite buttonBar = new Composite(comp, SWT.NONE);
+		// save panel
+		Composite saveSection = new Composite(comp, SWT.NONE);
+		saveSection.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		GridLayout saveLayout = new GridLayout(1, false);
+		saveSection.setLayout(saveLayout);
+
+		saveDialog = new CompositePresentationSaveDialog(comp.getShell());
+		Composite saveInput = new Composite(saveSection, SWT.NONE);
+		saveInput.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		saveDialog.createUI(saveInput);
+		saveDialog.createButtons(saveSection);
+
+		/*Composite buttonBar = new Composite(comp, SWT.NONE);
 		GridData data = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
 		data.verticalIndent = 15;
 		buttonBar.setLayoutData(data);
@@ -152,6 +167,7 @@ public class CompositePresentationDialog extends Dialog {
 
 		// TODO contestText.setFocus();
 		comp.getShell().setDefaultButton(cancel);
+		 */
 	}
 
 	private void createPresentationSection(Composite parent2) {
@@ -237,10 +253,12 @@ public class CompositePresentationDialog extends Dialog {
 		if (properties != null && properties.length() > 0)
 			newInfo.setData(properties);
 		buildList.add(newInfo);
+		enableOk();
 	}
 
 	protected void enableOk() {
-		ok.setEnabled(!buildList.getPresentationInfos().isEmpty());
+		boolean hasPresentations = !buildList.getPresentationInfos().isEmpty();
+		saveDialog.setPresentationsOk(hasPresentations);
 	}
 
 	private static Button createButton(Composite parent, String text, String tooltip) {
@@ -358,6 +376,7 @@ public class CompositePresentationDialog extends Dialog {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				buildList.clear();
+				enableOk();
 			}
 		});
 		buildList.addSelectionListener(new SelectionAdapter() {
