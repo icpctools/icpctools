eb6dd62da923cc6fd062873552eaf38a158e397a
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2025-05-24 17:17:45 -0400

    Show runs for spectators as per new access policy

--- ContestModel/src/org/icpc/tools/contest/model/internal/account/AnalystContest.java
+++ ContestModel/src/org/icpc/tools/contest/model/internal/account/AnalystContest.java
@@ -25,51 +25,6 @@ public class AnalystContest extends SpectatorContest {
 	}
 
 	@Override
-	public void add(IContestObject obj) {
-		if (obj instanceof IDelete) {
-			addIt(obj);
-			return;
-		}
-
-		IContestObject.ContestType cType = obj.getType();
-		switch (cType) {
-			case RUN: { // TODO - access block for live
-				IRun run = (IRun) obj;
-
-				IJudgement j = getJudgementById(run.getJudgementId());
-				if (j == null)
-					return;
-
-				if (isJudgementHidden(j))
-					return;
-
-				ISubmission s = getSubmissionById(j.getSubmissionId());
-				if (s == null)
-					return;
-
-				// hide runs for submissions outside the contest time
-				long time = s.getContestTime();
-				if (time < 0 || time >= getDuration())
-					return;
-
-				// hide runs for submissions after freeze
-				if (getFreezeDuration() != null) {
-					long freezeTime = getDuration() - getFreezeDuration();
-					if (s.getContestTime() >= freezeTime && getState().getThawed() == null) {
-						freeze.add(run);
-						return;
-					}
-				}
-
-				addIt(run);
-				return;
-			}
-			default:
-				super.add(obj);
-		}
-	}
-
-	@Override
 	protected IPerson filterPerson(IPerson person) {
 		Person p = (Person) ((Person) person).clone();
 		p.add("email", null);
