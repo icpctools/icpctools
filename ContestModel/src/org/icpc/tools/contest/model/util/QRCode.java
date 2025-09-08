aed632b7ea6a2e64f7e10598fefc49f57d811df2
Tim deBoer <git@tdeboer.ca>
2025-09-08 13:47:43 -0400

    QR code generator util
    Added a main() method to the QR code generator to make it easy to generate codes.
    Just run it with two arguments: the QR code to generate and the filename to use.
    An image with that QR code will be generated with that name in the working folder.
    
    Also documented a few handy codes for our current scanners.

--- ContestModel/src/org/icpc/tools/contest/model/util/QRCode.java
+++ ContestModel/src/org/icpc/tools/contest/model/util/QRCode.java
@@ -1,9 +1,21 @@
 package org.icpc.tools.contest.model.util;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.icpc.tools.contest.Trace;
 
 import io.nayuki.qrcodegen.QrCode;
 
+/**
+ * Some handy QR codes from the readers we used in Baku: Battery life: ^&037&^, Reset to default:
+ * ^&002&^, Volume off: ^&03A&^, Vibrate on: ^&039&^.
+ */
 public class QRCode {
 	/**
 	 * Draws a QR code representing the given text in a square at x, y, with height & width s.
@@ -20,5 +32,27 @@ public class QRCode {
 				}
 			}
 		}
+	}
+
+	/*
+	 * Test QR codes by creating the first command line args as a QR code and writing it to the second arg as a filament.
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args == null || args.length != 2) {
+			Trace.trace(Trace.ERROR,
+					"Arguments should be [QRcode] [filename], e.g. '0 zero' which will create zero.png with a QR code containing the number 0");
+			return;
+		}
+		int size = 1000;
+
+		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = (Graphics2D) image.getGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, size, size);
+		g.setColor(Color.BLACK);
+		QRCode.drawQRCode(g, args[0], 0, 0, size);
+		g.dispose();
+		File f = new File(args[1] + ".png");
+		ImageIO.write(image, "png", f);
 	}
 }
