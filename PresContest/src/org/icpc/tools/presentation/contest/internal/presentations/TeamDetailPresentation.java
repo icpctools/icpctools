34cad0c257eff0b7b9fe434e747351bae708545f
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2026-02-28 10:36:39 +0100

    Fix windows audio

--- PresContest/src/org/icpc/tools/presentation/contest/internal/presentations/TeamDetailPresentation.java
+++ PresContest/src/org/icpc/tools/presentation/contest/internal/presentations/TeamDetailPresentation.java
@@ -12,6 +12,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
 
 import org.icpc.tools.contest.Trace;
 import org.icpc.tools.contest.model.IOrganization;
@@ -109,25 +110,19 @@ public class TeamDetailPresentation extends AbstractICPCPresentation {
 			AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
 			Clip clip = AudioSystem.getClip();
 
-			clip.open(audioIn);
-			clip.start();
-
-			// clean up clip when it's done
-			execute(new Runnable() {
-				@Override
-				public void run() {
+			clip.addLineListener(event -> {
+				if (event.getType() == LineEvent.Type.STOP) {
+					clip.close();
 					try {
-						while (clip.isRunning()) {
-							Thread.sleep(100);
-						}
-
-						clip.close();
 						audioIn.close();
 					} catch (Exception e) {
 						// ignore
 					}
 				}
 			});
+
+			clip.open(audioIn);
+			clip.start();
 		} catch (Exception e) {
 			Trace.trace(Trace.ERROR, "Could not play wav file " + file.getAbsolutePath(), e);
 		}
