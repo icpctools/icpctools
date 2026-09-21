eb91efc32abdcb76318335468ea4250ba40f1fbd
Tim deBoer <git@tdeboer.ca>
2026-09-21 10:06:00 -0400

    fix: missing tokens in event feed
    The event feed was missing auth tokens because it uses async background threads
    to process every new set of events.
    
    This caches the account token before starting the feed, and then correctly sets
    it (or clears it for un-authenticated users) before sending events.
    
    Since I was working in this area I also fixed a few other minor issues:
    - Only set the thread host and account token if we're going to use them; only
      flush if we've sent events. ('if there are no events, do nothing')
    - The heartbeat was being sent after 120 counts, but count was dropped from the
      original 1s down to 250ms so heartbeat is now sent every 30s. Changed it
      to be time-based and sent every 100s. Also flushes immediately.
    - Reduced the startup delay so feeds start 2.5s after CDS starts instead of 5s.
    - Mostly paranoia, but I also explicitly cleared the thread account token for
      other paths in case an HTTP thread is reused to respond to multiple clients.
    
    Fixes #1349.

--- CDS/src/org/icpc/tools/cds/service/ContestFeedExecutor.java
+++ CDS/src/org/icpc/tools/cds/service/ContestFeedExecutor.java
@@ -20,7 +20,7 @@ public class ContestFeedExecutor {
 	}
 
 	protected void start(ScheduledExecutorService executor) {
-		executor.scheduleAtFixedRate(() -> output(), 5000, 250, TimeUnit.MILLISECONDS);
+		executor.scheduleAtFixedRate(() -> output(), 2500, 250, TimeUnit.MILLISECONDS);
 	}
 
 	public void output() {
